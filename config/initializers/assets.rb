65ba26119343387b1c5fab12f3fc70b372b0b4a9
Dan Mitchell <dan@danmitchell.co.uk>
2018-11-09 17:14:55 +0000

    Add govuk frontend

--- config/initializers/assets.rb
+++ config/initializers/assets.rb
@@ -3,10 +3,20 @@
 # Version of your assets, change this if you want to expire all your assets.
 Rails.application.config.assets.version = '1.0'
 
-# Add additional assets to the asset load path.
-# Rails.application.config.assets.paths << Emoji.images_path
+# Add GOVUK image asset path
+Rails.application.config.assets.paths << Rails.root.join("node_modules", "govuk-frontend", "assets", "images")
 
-# Precompile additional assets.
-# application.js, application.css, and all non-JS/CSS in the app/assets
-# folder are already added.
-# Rails.application.config.assets.precompile += %w( admin.js admin.css )
+# Add GOVUK font asset path
+Rails.application.config.assets.paths << Rails.root.join("node_modules", "govuk-frontend", "assets", "fonts")
+
+Rails.application.config.assets.precompile += %w[
+  favicon.ico
+  govuk-mask-icon.svg
+  govuk-apple-touch-icon-180x180.png
+  govuk-apple-touch-icon-167x167.png
+  govuk-apple-touch-icon-152x152.png
+  govuk-apple-touch-icon.png
+  govuk-opengraph-image.png
+  govuk-logotype-crown.png
+  govuk-crest-2x.png
+]

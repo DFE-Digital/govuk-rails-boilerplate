0be3b5b91beb7f884434075135eb08f082af8737
Dan Mitchell <dan@danmitchell.co.uk>
2018-12-17 08:46:42 +0000

    Add custom error pages
    These are an exact replacement for the normal rails pages.
    The content should be made specific for each service but for this boilerplate it's generic .

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,7 @@
 Rails.application.routes.draw do
   get "/pages/:page", to: "pages#show"
+
+  get "/404", to: "errors#not_found", via: :all
+  get "/422", to: "errors#unprocessable_entity", via: :all
+  get "/500", to: "errors#internal_server_error", via: :all
 end

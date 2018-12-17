c6ea1e5a36bf8bfd9228a3bc098efd84d642bdcb
Dan Mitchell <dan@danmitchell.co.uk>
2018-12-17 08:46:42 +0000

    Add custom error pages
    These are an exact replacement for the normal rails pages.
    The content should be made specific for each service but for this boilerplate it's generic .

---
+++ spec/controllers/errors_controller_spec.rb
@@ -0,0 +1,24 @@
+require 'rails_helper'
+
+RSpec.describe ErrorsController, type: :controller do
+  describe 'GET #not_found' do
+    it 'returns not found' do
+      get :not_found
+      expect(response).to have_http_status(:not_found)
+    end
+  end
+
+  describe 'GET #internal_server_error' do
+    it 'returns internal_server_error' do
+      get :internal_server_error
+      expect(response).to have_http_status(:internal_server_error)
+    end
+  end
+
+  describe 'GET #unprocessable_entity' do
+    it 'returns unprocessable_entity' do
+      get :unprocessable_entity
+      expect(response).to have_http_status(:unprocessable_entity)
+    end
+  end
+end
